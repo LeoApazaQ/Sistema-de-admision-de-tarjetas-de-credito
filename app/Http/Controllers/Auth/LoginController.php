eda2cfb54593d30fd81fcd7c4edac00229e8baa2
LeoApazaQ <145884900+LeoApazaQ@users.noreply.github.com>
2024-11-21 10:10:21 -0500

    corrigiendo error de redireccion de login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
@@ -37,4 +39,18 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
         $this->middleware('auth')->only('logout');
     }
+
+    protected function authenticated(Request $request, $user)
+    {
+        // Ejemplo: redirigir a una ruta específica según el tipo de usuario
+        /* if ($user->is_admin) {
+            return redirect()->route('admin.dashboard');
+        } */
+
+        // Redirigir a la página principal
+        return redirect()->route('home');
+    }
+
+
+
 }
